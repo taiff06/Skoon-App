d780b6e026d970927c1c07a14350fdabc99526a9
taif <taif_999_7@hotmail.com>
2023-12-19 11:46:52 +0300

    Initial Commit

---
+++ Skoon Watch App/SkoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkoonApp.swift
+//  Skoon Watch App
+//
+//  Created by taif on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Skoon_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
